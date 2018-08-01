b9180709d8e6279cceef5c94094463c389e99c2b
jaydeep apte <jaydeepresources@gmail.com>
2018-08-01 15:16:52 +0530

    Add files via upload

---
+++ Filters/src/MyFilter.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class MyFilter implements Filter {
+
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("init");
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpSession session = ((HttpServletRequest) req).getSession();
+
+		String user = req.getParameter("name");
+		String password = req.getParameter("password");
+
+		if (user != null && password != null && password.equals("admin")) {
+			session.setAttribute("user", user);
+			System.out.println("Name in session=" + session.getAttribute("user"));
+			chain.doFilter(req, resp);// sends request to next resource
+		}
+
+		else {
+			RequestDispatcher rd = req.getRequestDispatcher("index.html");
+			rd.include(req, resp);
+		}	
+	}
+
+	public void destroy() {
+	}
+
+}
